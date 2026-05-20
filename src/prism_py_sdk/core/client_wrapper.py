cb5224645dd0f8ae9cbb5a61705bd09335d50b2d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-05-20 03:33:13 +0000

    Release 1.0.5

--- src/prism_py_sdk/core/client_wrapper.py
+++ src/prism_py_sdk/core/client_wrapper.py
@@ -27,12 +27,12 @@ class BaseClientWrapper:
         import platform
 
         headers: typing.Dict[str, str] = {
-            "User-Agent": "prism-py-sdk/1.0.4",
+            "User-Agent": "prism-py-sdk/1.0.5",
             "X-Fern-Language": "Python",
             "X-Fern-Runtime": f"python/{platform.python_version()}",
             "X-Fern-Platform": f"{platform.system().lower()}/{platform.release()}",
             "X-Fern-SDK-Name": "prism-py-sdk",
-            "X-Fern-SDK-Version": "1.0.4",
+            "X-Fern-SDK-Version": "1.0.5",
             **(self.get_custom_headers() or {}),
         }
         headers["X-Api-Key"] = self.api_key
